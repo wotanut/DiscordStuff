760f06eb158fb50967626cf4be6c7d0c89bc0b22
Sam <sambpilot@gmail.com>
2022-11-08 16:59:26 +0000

    READMEEE

--- plugins/removeTrackingURL/readme.md
+++ plugins/removeTrackingURL/readme.md
@@ -1,1 +1,11 @@
-# removes all the tracking urls from the links you send in discord
+<div style="text-align: center;">
+    <h1>removeTrackingURL</h1>
+    <p>Removes tracking URL from links</p>
+    <img alt="Discord" src="https://img.shields.io/discord/939479619587952640?logo=discord&style=for-the-badge">  <img alt="GitHub followers" src="https://img.shields.io/github/followers/wotanut?logo=github&style=for-the-badge">
+</div>
+
+<div style="text-align: center;">
+    <h2> What is this?</h2>
+    <p> This is a plugin that automatically removes trackers from supported urls. Notice when you go to twitter and you see a tweet you like, if you click the share button and you paste it in to discord there's a bunch of crap after the question mark. </p>
+    <img src="imgs/inital.png>" alt="initial"></img>
+</div>
--- plugins/removeTrackingURL/removeTrackingURL/index.js
+++ plugins/removeTrackingURL/removeTrackingURL/index.js
@@ -6,8 +6,8 @@
  */
 module.exports = (Plugin, Library) => {
 
-    const {DiscordModules, Logger, Patcher, WebpackModules, Settings, Toasts} = Library;
-    const {MessageStore, UserStore, ImageResolver, ChannelStore, Dispatcher, MessageActions} = DiscordModules;
+    const {DiscordModules, Logger, Patcher, Settings, Toasts} = Library;
+    const {MessageActions,} = DiscordModules;
 
     
     return class extends Plugin {
@@ -24,67 +24,52 @@ module.exports = (Plugin, Library) => {
         onStart() {
             Logger.info("Enabling removeTrackingURL!");
 
-            Patcher.before(Dispatcher, "dispatch", (_, args) => {
-                var event = args[0]
+            Patcher.before(DiscordModules.MessageActions, "sendMessage", (t,a) => {
+                var msgcontent = a[1].content
+                // twitter
 
-                // Logger.info(event.type);
+                // example of a twitter link 
+                // https://twitter.com/SoVeryBritish/status/1555115704839553024?s=20&t=a2A24ImVWWDElGic3hTwNg
 
-                if (event.type === "MESSAGE_CREATE") {
-                    const message = event.message;
+            
+                if (this.settings.twitter) {
+                    if (msgcontent.includes("https://twitter.com")) {
 
-                    if (message.author.id !== UserStore.getCurrentUser().id) return;
+                        // if it includes the twitter url then it'll flow down here and appropriately remove the trackers and update the url.
+                        // note: for those of you who /care/ so much about speed you will get a very slight performance increase if you use FXtwitter.
 
-                    Logger.info(event)
+                        const tweet = /(https:\/\/twitter.com\/\w+\/status\/\d+)/g.exec(msgcontent)[0];
+                        msgcontent = msgcontent.replace(/(https:\/\/twitter.com\/\w+\/status\/\d+\?[a-zA-Z0-9=&]*)/g, tweet);
 
-                    // at this point we KNOW that the message is created by the current user and so we can edit it if it meets the requirements.
-
-                    // twitter
-
-                    // example of a twitter link 
-                    // https://twitter.com/SoVeryBritish/status/1555115704839553024?s=20&t=a2A24ImVWWDElGic3hTwNg
-
-                
-                    if (this.settings.twitter) {
-                        if (message.content.includes("https://twitter.com")) {
-
-                            // if it includes the twitter url then it'll flow down here and appropriately remove the trackers and update the url.
-                            // note: for those of you who /care/ so much about speed you will get a very slight performance increase if you use FXtwitter.
-
-                            // regex for tweet urls (https:\/\/twitter.com\/\w+\/status\/\d+)
-
-                            const tweet = /(https:\/\/twitter.com\/\w+\/status\/\d+)/g.exec(message.content)[0];
-                            message.content = message.content.replace(/(https:\/\/twitter.com\/\w+\/status\/\d+\?[a-zA-Z0-9=&]*)/g, tweet);
-
-                            if (this.settings.FXtwitter) {
-                                event.message.content = message.content.replace("https://twitter.com", "https://fxtwitter.com");
-                            }
-                            else if(this.settings.VXtwitter) {
-                                event.message.content = message.content.replace("https://twitter.com", "https://c.vxtwitter.com");
-                            }
-
-                            // locally edits messages, but we want to edit message server side
-                            // MessageActions.editMessage("test");
-                            Toasts.success("Succesfully removed tracker from twitter link!");
+                        if (this.settings.FXtwitter) {
+                            msgcontent = msgcontent.replace("https://twitter.com", "https://fxtwitter.com");
                         }
-                    }
-
-                    // reddit
-
-                    // example of a reddit link 
-                    //  https://www.reddit.com/r/GCSE/comments/kv1pny/leak_of_gcse_algorithm_to_find_grades/?utm_source=share&utm_medium=web2x&context=3
-
-                    if (this.settings.reddit) {
-                        if (message.content.includes("https://reddit.com")){
-                            const post = /(https:\/\/reddit.com\/r\/\w+\/comments\/\w+\/\w+\/)/g.exec(message.content)[0];
-
-                            message.content = message.content.replace(/(https:\/\/reddit.com\/r\/\w+\/comments\/\w+\/\w+\/\?[a-zA-Z0-9=&]*)/g, post);
-
-                            Logger.info("Reddit link detected. Updating message from " + message + " to -> " + new_message);
-                            Toasts.success("Succesfully removed tracker from reddit link!");
+                        else if(this.settings.VXtwitter) {
+                            msgcontent = msgcontent.replace("https://twitter.com", "https://c.vxtwitter.com");
                         }
+                        Toasts.success("Succesfully removed tracker from twitter link!");
                     }
                 }
-            })
+
+                // reddit
+
+                // example of a reddit link 
+                //  https://www.reddit.com/r/GCSE/comments/kv1pny/leak_of_gcse_algorithm_to_find_grades/?utm_source=share&utm_medium=web2x&context=3
+
+                if (this.settings.reddit) {
+                    if (msgcontent.includes("https://www.reddit.com")){
+                        const post = /(https:\/\/www.reddit.com\/r\/\w+\/comments\/\w+\/\w+\/)/g.exec(msgcontent)[0];
+
+                        msgcontent = msgcontent.replace(/(https:\/\/www.reddit.com\/r\/\w+\/comments\/\w+\/\w+\/[_=&a-z1-9]*)/g, post);
+
+                        Toasts.success("Succesfully removed tracker from reddit link!");
+                    }
+                }
+
+                // Changes our new message back to the original message
+                a[1].content = msgcontent;
+
+            });
         }
 
         onStop() {
