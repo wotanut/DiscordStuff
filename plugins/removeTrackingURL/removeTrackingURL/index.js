338daeb3b421f815ec2faf0610ac40facccb3286
Sam <sambpilot@gmail.com>
2022-11-08 14:46:12 +0000

    Finally got the logic implemented

--- plugins/removeTrackingURL/removeTrackingURL/index.js
+++ plugins/removeTrackingURL/removeTrackingURL/index.js
@@ -7,7 +7,7 @@
 module.exports = (Plugin, Library) => {
 
     const {DiscordModules, Logger, Patcher, WebpackModules, Settings, Toasts} = Library;
-    const {MessageStore, UserStore, ImageResolver, ChannelStore, Dispatcher} = DiscordModules;
+    const {MessageStore, UserStore, ImageResolver, ChannelStore, Dispatcher, MessageActions} = DiscordModules;
 
     
     return class extends Plugin {
@@ -25,37 +25,64 @@ module.exports = (Plugin, Library) => {
             Logger.info("Enabling removeTrackingURL!");
 
             Patcher.before(Dispatcher, "dispatch", (_, args) => {
-                const event = args[0]
+                var event = args[0]
+
+                // Logger.info(event.type);
 
                 if (event.type === "MESSAGE_CREATE") {
                     const message = event.message;
-                    const user = UserStore.getUser(message.author.id);
 
-                    if (user !== UserStore.getCurrentUser()) return;
+                    if (message.author.id !== UserStore.getCurrentUser().id) return;
+
+                    Logger.info(event)
 
                     // at this point we KNOW that the message is created by the current user and so we can edit it if it meets the requirements.
+
+                    // twitter
 
                     // example of a twitter link 
                     // https://twitter.com/SoVeryBritish/status/1555115704839553024?s=20&t=a2A24ImVWWDElGic3hTwNg
 
-                    // twitter
+                
                     if (this.settings.twitter) {
                         if (message.content.includes("https://twitter.com")) {
 
-                            var new_message = message.content.replace(/(https:\/\/twitter.com\/\w+\/status\/\d+)(\?s=\d+&t=\w+)/g, "$1");
+                            // if it includes the twitter url then it'll flow down here and appropriately remove the trackers and update the url.
+                            // note: for those of you who /care/ so much about speed you will get a very slight performance increase if you use FXtwitter.
 
-                            message.content = message.content.split("?")[0];
-                            // MessageStore._sendMessage(message.content)
-                            Logger.info("Twitter link detected. Updating message from " + message.content + " to -> " + new_message);
-                            // MessageStore.updateMessage(channel, message);
+                            // regex for tweet urls (https:\/\/twitter.com\/\w+\/status\/\d+)
+
+                            const tweet = /(https:\/\/twitter.com\/\w+\/status\/\d+)/g.exec(message.content)[0];
+                            message.content = message.content.replace(/(https:\/\/twitter.com\/\w+\/status\/\d+\?[a-zA-Z0-9=&]*)/g, tweet);
+
+                            if (this.settings.FXtwitter) {
+                                event.message.content = message.content.replace("https://twitter.com", "https://fxtwitter.com");
+                            }
+                            else if(this.settings.VXtwitter) {
+                                event.message.content = message.content.replace("https://twitter.com", "https://c.vxtwitter.com");
+                            }
+
+                            // locally edits messages, but we want to edit message server side
+                            // MessageActions.editMessage("test");
                             Toasts.success("Succesfully removed tracker from twitter link!");
                         }
                     }
 
                     // reddit
 
-                    return;
+                    // example of a reddit link 
+                    //  https://www.reddit.com/r/GCSE/comments/kv1pny/leak_of_gcse_algorithm_to_find_grades/?utm_source=share&utm_medium=web2x&context=3
 
+                    if (this.settings.reddit) {
+                        if (message.content.includes("https://reddit.com")){
+                            const post = /(https:\/\/reddit.com\/r\/\w+\/comments\/\w+\/\w+\/)/g.exec(message.content)[0];
+
+                            message.content = message.content.replace(/(https:\/\/reddit.com\/r\/\w+\/comments\/\w+\/\w+\/\?[a-zA-Z0-9=&]*)/g, post);
+
+                            Logger.info("Reddit link detected. Updating message from " + message + " to -> " + new_message);
+                            Toasts.success("Succesfully removed tracker from reddit link!");
+                        }
+                    }
                 }
             })
         }
