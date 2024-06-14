ee517ff07121702da359b0aaf0e83cc1372e97cd
Firefly <firejet707@gmail.com>
2024-06-13 22:07:09 -0400

    add Youtube

--- plugins/src/removeTrackingURL/index.js
+++ plugins/src/removeTrackingURL/index.js
@@ -76,7 +76,8 @@ module.exports = (Plugin, Library) => {
     const DEFAULT_SITES = {
         "twitter": new Site("Twitter/X", true, ["twitter.com", "x.com"], /(.+)/g, /((https?:\/\/)?(www\.)?(twitter|x).com\/\w+\/status\/\d+\?\S+)/g, null),
         "reddit": new Site("Reddit", true, ["reddit.com"], /(.+)/g, /((https?:\/\/)?(www\.)?reddit\.com\/\S+)/g, null),
-        "spotify": new Site("Spotify", true, ["open.spotify.com"], /(.+)/g, /((https?:\/\/)?(www\.)?open\.spotify\.com\/(track|album|user|artist|playlist)\/\w+\?\S+)/g, null)
+        "spotify": new Site("Spotify", true, ["open.spotify.com"], /(.+)/g, /((https?:\/\/)?(www\.)?open\.spotify\.com\/(track|album|user|artist|playlist)\/\w+\?\S+)/g, null),
+        "youtube": new Site("Youtube", true, ["youtu.be"], /(si)/g, /((https?:\/\/)?(www\.)?youtu.be/\w+)?.+/g, null)
     }
 
     // I'm hoping to add ability for custom sites to be added soon.
